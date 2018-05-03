e3ac063cc3d38dd612a8d1ecf9cb72ce8ee4e77b
Szymon Piórkowski <szymmon123@gmail.com>
2018-05-03 15:33:20 +0200

    Added project files.

---
+++ Assets/Scripts/SetPositiontoMousePosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositiontoMousePosition : MonoBehaviour {
+
+    Transform trans;
+
+	// Use this for initialization
+	void Start () {
+        trans = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+	}
+}
